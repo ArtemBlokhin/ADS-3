6c4710d44519de5609814def2801de7f512904bf
ArtemBlokhin <75878060+ArtemBlokhin@users.noreply.github.com>
2021-04-27 18:44:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,95 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include "tstack.h"
-
-std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
-  return std::string("");
+int priority(char symbol){
+  switch(symbol){
+    case '(':
+      return 0;
+      break;
+    case ')':
+      return 1;
+      break;
+    case '+':
+      return 2;
+      break;
+    case '-':
+      return 2;
+      break;
+    case '*':
+      return 3;
+      break;
+    case '/':
+      return 3;
+      break;
+  }
 }
 
+
+std::string infx2pstfx(std::string inf) {
+  std::string NewStr;
+  std::string NewStrPr;
+  TStack <char> StackOfNumbers;
+  for(int i = 0;i<inf.lenght();i++){
+    if(priority(inf[i]) == -1){
+      NewStr += inf[i] + " ";                
+    }
+    else if(priority(inf[i]) == 0){
+      StackOfNumbers.push(inf[i]);        
+    }
+    else if(priority(inf[i]) == 1){
+      while(StackOfNumbers.get() != '(')  {
+        NewStr += StackOfNumbers.get() + " ";  
+        StackOfNumbers.pop();       
+      } 
+      StackOfNumbers.pop();          
+    }
+    else{
+      while(!StackOfNumbers.isEmpty() && priority(StackOfNumbers.get()) >= priority(inf[i])){
+          str += StackOfNumbers.get() + " ";
+          StackOfNumbers.pop();
+      }
+      StackOfNumbers.push();            
+    }
+  }
+  while(!StackOfNumbers.isEmpty()){
+   NewStr += StackOfNumbers.get(); 
+   StackOfNumbers.pop();   
+  }
+  NewStrPr = NewStr.substring(0,NewStr.lenght() - 1);
+  return NewStrPr;
+}
+int calculation(int first,int second,char symbol){
+  switch(symbol){
+    case '+':
+      return first + second;
+      break;
+    case '-':
+      return first - second;
+      break;
+    case '*':
+      return first * second;
+      break;
+    case '/':
+      return first / second;
+      break;       
+  }
+}
 int eval(std::string pst) {
-  // добавьте сюда нужный код
-  return 0;
+  TStack <char> StackOfNumbers;  
+  for(int i = 0;i<pst.lenght();i++){
+    int charpr = priority(pst[i]);
+    if(pst[i] <= '9' && pst[i] >= '0'){
+      StackOfNumbers.push(pst[i] - '0'); 
+    }
+    else if (pst[i] != ' ') {
+      int first = StackOfNumbers.get();
+      StackOfNumbers.pop();
+      int second = StackOfNumbers.get();
+      StackOfNumbers.pop();
+      int result = calculation(first,second,pst[i]);
+      StackOfNumbers.push(result);                 
+    }   
+  }
+  int result = StackOfNumbers.get();
+  return result;  
 }
